93185e20082aa8b3a8ebe3f7fb1740071bda3d72
happyland-support <80957652+happyland-support@users.noreply.github.com>
2022-02-18 21:07:11 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,54 @@
 'use strict';
 {
 
+  /* Intersection Observer API */
+
+  function inViewCallback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  function onScrollCallback(entries) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        header.classList.add('scrolled');
+        toTop.classList.add('scrolled');
+      } else {
+        header.classList.remove('scrolled');
+        toTop.classList.remove('scrolled');
+      }
+    });
+  }
+
+  const header = document.querySelector('header');
+  const toTop = document.getElementById('to_top');
+
+  const inViewObserver = new IntersectionObserver(inViewCallback, {
+    threshold: 0.2,
+  });
+
+  document.querySelectorAll('.animate').forEach(el => {
+    inViewObserver.observe(el);
+  });
+
+  const onScrollObserver = new IntersectionObserver(onScrollCallback);
+  onScrollObserver.observe(document.getElementById('target'));
+
+  toTop.addEventListener('click', e => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+
+  /* メニュー */
+
   class Menu {
     constructor() {
       this.open = document.getElementById('open');
@@ -11,13 +59,11 @@
     addListeners() {
       this.open.addEventListener('click', () => {
         this.overlay.classList.add('show');
-        this.open.classList.add('hide');
         this.mask.classList.remove('disable');
       });
     
       this.close.addEventListener('click', () => {
         this.overlay.classList.remove('show');
-        this.open.classList.remove('hide');
         this.mask.classList.add('disable');
       });
     
